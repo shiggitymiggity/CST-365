52f0790bedb3d5ceff6b1802ccca2f33b999d421
Bowen <34555759+shiggitymiggity@users.noreply.github.com>
2020-01-27 18:29:26 -0800

    Update students.js

---
+++ Week3/js/students.js
@@ -0,0 +1,7 @@
+var students = '{"students": [{"id": "Student ID", "email": "Email Address" },\
+ {"id": 356785, "email": "Thisoneemail@gmal.com"}\
+ {"id": 164857, "email": "Anotherstudent@hotmail.com"}\
+ {"id": 201385, "email": "Emailemail@email.com"}\
+ {"id": 972035, "email": "Mynbox@protonmail.com"}\
+ {"id": 701803, "email": "Lastone@gmail.com"}\
+]}'
