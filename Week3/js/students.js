a7b243c2b46f457723a9a1332b1a6673bbdceb13
Bowen <34555759+shiggitymiggity@users.noreply.github.com>
2020-01-27 18:48:17 -0800

    Adding functions

--- Week3/js/students.js
+++ Week3/js/students.js
@@ -5,3 +5,27 @@ var students = '{"students": [{"id": "Student ID", "email": "Email Address" },\
  {"id": 972035, "email": "Mynbox@protonmail.com"}\
  {"id": 701803, "email": "Lastone@gmail.com"}\
 ]}'
+
+
+function returnStudents() 
+{
+    return JSON.parse(students);
+}
+
+function populateTable()
+{
+    var data = returnStudents();
+    displayStudents(data.students);
+}
+
+function displayStudents(input) {
+    var tableList = "";
+    for(i = 0; i < input.length; i++) {
+        var name = input[i].id;
+        var price = input[i].email;
+        tableList += "<tr><td>" + id + "</td><td>" + email + "</td>";
+
+        document.getElementById("product-list").innerHTML = tableList;
+    }
+}
+
