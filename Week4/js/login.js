3375368121af4b26ac2ceff4bebf1dba51a8949b
Bowen <34555759+shiggitymiggity@users.noreply.github.com>
2020-02-03 15:39:01 -0800

    it appears it's not using my css after the gulp

--- Week4/js/login.js
+++ Week4/js/login.js
@@ -3,8 +3,8 @@ function validate() {
     document.getElementById('name-error').style.visibility = 'hidden'
     document.getElementById('password-error').style.visibility = 'hidden'
 
-    let firstName = document.getElementById('name').value
-    let lastName = document.getElementById('password').value
+    var firstName = document.getElementById('name').value
+    var lastName = document.getElementById('password').value
 
     if (firstName == '') {
         document.getElementById('name-error').style.visibility = 'visible'
