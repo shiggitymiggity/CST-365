b5d104868102c79a0988b8784beac765569b8f9f
Bowen <34555759+shiggitymiggity@users.noreply.github.com>
2020-02-11 01:45:21 -0800

    it crashes...

---
+++ Week5/webapp/src/components/Home.vue
@@ -0,0 +1,13 @@
+<template>
+ <h1>Hello World</h1>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
