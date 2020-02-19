e4189e53b83e225a00a9b1ff4d87a719f7af15be
Bowen <34555759+shiggitymiggity@users.noreply.github.com>
2020-02-19 14:29:04 -0800

    Got student API to work on to Instructor

--- Week6/webapi/Database/InMemory.cs
+++ Week6/webapi/Database/InMemory.cs
@@ -7,13 +7,28 @@ namespace Database
         public static List<Student> Students = new List<Student> {
             new Student {
                 Id = 1,
-                StudentId = "100",
-                EmailAddress = "tom.smith@school.com"
+                StudentId = "356785",
+                EmailAddress = "Thisoneemail@gmal.com"
             },
             new Student {
                 Id = 2,
-                StudentId = "200",
-                EmailAddress = "mary.jones@school.com"
+                StudentId = "164857",
+                EmailAddress = "Anotherstudent@hotmail.com"
+            },
+            new Student {
+                Id = 3,
+                StudentId = "201385",
+                EmailAddress = "Emailemail@email.com"
+            },
+            new Student {
+                Id = 4,
+                StudentId = "972035",
+                EmailAddress = "MyInbox@protonmail.com"
+            },
+            new Student {
+                Id = 5,
+                StudentId = "701803",
+                EmailAddress = "Lastone@gmail.com"
             }
         };
     }
--- Week6/webapi/Models/Student.cs
+++ Week6/webapi/Models/Student.cs
@@ -1,6 +1,8 @@
+
     public class Student
     {
         public int Id { get; set; }
         public string StudentId { get; set; }
         public string EmailAddress { get; set; }
     }
+    
