e48f556242cfb0b8879d3cb6346dd286ed831595
Bowen <34555759+shiggitymiggity@users.noreply.github.com>
2020-02-26 13:08:26 -0800

    Week 7 Exercise 2

---
+++ Week7/webapi/Database/InMemory.cs
@@ -0,0 +1,68 @@
+using System.Collections.Generic;
+
+namespace Database
+{
+    public static class InMemory
+    {
+        public static List<Student> Students = new List<Student> {
+            new Student {
+                Id = 1,
+                StudentId = "356785",
+                EmailAddress = "Thisoneemail@gmal.com"
+            },
+            new Student {
+                Id = 2,
+                StudentId = "164857",
+                EmailAddress = "Anotherstudent@hotmail.com"
+            },
+            new Student {
+                Id = 3,
+                StudentId = "201385",
+                EmailAddress = "Emailemail@email.com"
+            },
+            new Student {
+                Id = 4,
+                StudentId = "972035",
+                EmailAddress = "MyInbox@protonmail.com"
+            },
+            new Student {
+                Id = 5,
+                StudentId = "701803",
+                EmailAddress = "Lastone@gmail.com"
+            }
+        };
+
+            public static List<Instructor> Instructors = new List<Instructor> {
+            new Instructor {
+                Id = 1,
+                First = "Kajetan",
+                Middle = "A.",
+                Last = "Savage"
+            },
+            new Instructor {
+                Id = 2,
+                First = "Evelina",
+                Middle = "T.",
+                Last = "Almond"
+            },
+            new Instructor {
+                Id = 3,
+                First = "Orlaith",
+                Middle = "L.",
+                Last = "Wilde"
+            },
+            new Instructor {
+                Id = 4,
+                First = "Alfie",
+                Middle = "Q.",
+                Last = "Santiago"
+            },
+            new Instructor {
+                Id = 5,
+                First = "Gail",
+                Middle = "U.",
+                Last = "Stark"
+            }
+        };
+    }
+}
---
+++ Week7/webapp/src/components/Students.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <h2 align="center" class='section-heading'>Students</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID Number</th>
+                    <th>Email</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="student in students" v-bind:key="student">
+                    <td>{{ student.id }}</td>
+                    <td>{{ student.email }}</td>                   
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Tables',
+        mounted() {
+            this.students = getStudents();
+        },
+        data () {
+            return {
+                students: []
+            }
+        }
+    }
+    function getStudents() {
+        return JSON.parse(students);
+    }
+
+    var students = '[{"id": 356785, "email": "Thisoneemail@gmal.com"},\
+                    {"id": 164857, "email": "Anotherstudent@hotmail.com"},\
+                    {"id": 201385, "email": "Emailemail@email.com"},\
+                    {"id": 972035, "email": "Mynbox@protonmail.com"},\
+                    {"id": 701803, "email": "Lastone@gmail.com"}]'
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
